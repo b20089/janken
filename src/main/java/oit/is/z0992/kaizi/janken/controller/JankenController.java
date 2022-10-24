6224a1fac59b60c47526b514c96d8b4e1f611373
b20089 <e1b20089@oit.ac.jp>
2022-10-24 23:01:39 +0900

    DBのユーザ情報と試合結果を表示できるようにした

--- src/main/java/oit/is/z0992/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0992/kaizi/janken/controller/JankenController.java
@@ -1,10 +1,9 @@
 package oit.is.z0992.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,8 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//import大事
 import oit.is.z0992.kaizi.janken.model.Entry;
+import oit.is.z0992.kaizi.janken.model.Match;
+import oit.is.z0992.kaizi.janken.model.MatchMapper;
+import oit.is.z0992.kaizi.janken.model.User;
+import oit.is.z0992.kaizi.janken.model.UserMapper;
 
 /**
  * sample21Controller
@@ -25,17 +28,21 @@ public class JankenController {
 
   @Autowired
   private Entry entry;
+
+  @Autowired
+  UserMapper userMapper;
+
+  @Autowired
+  MatchMapper matchMapper;
   /**
    * work02part1というGETリクエストがあったら work02part1()を呼び出し，work02part1.htmlを返す
-*/
-/*
-  @GetMapping("/janken")
-  public String janken() {
-    return "janken.html";
-  }
-*/
-
-
+   */
+  /*
+   * @GetMapping("/janken")
+   * public String janken() {
+   * return "janken.html";
+   * }
+   */
 
   /**
    * パスパラメータ2つをGETで受け付ける 1つ目の変数をparam1という名前で，2つ目の変数をparam2という名前で受け取る
@@ -50,33 +57,29 @@ public class JankenController {
   @GetMapping("/janken1/{param1}")
   public String janken(@PathVariable String param1, ModelMap model) {
     int i = Integer.parseInt(param1);// param1が文字列なので，parseIntでint型の数値に変換する
-    //グー：１，チョキ：２，パー：３
+    // グー：１，チョキ：２，パー：３
     String te = null, result = null;
-    if(i == 1){
-        te = "Gu";
-        result = "draw...";
-    }
-    else if(i == 2){
-        te = "Choki";
-        result = "You Lose...";
-    }
-    else if(i == 3){
-        te = "Pa";
-        result = "You Win!";
+    if (i == 1) {
+      te = "Gu";
+      result = "draw...";
+    } else if (i == 2) {
+      te = "Choki";
+      result = "You Lose...";
+    } else if (i == 3) {
+      te = "Pa";
+      result = "You Win!";
     }
     // ModelMap型変数のmodelにtasuResult1という名前の変数で，tasuResultの値を登録する．
     // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     model.addAttribute("te", te);
     model.addAttribute("result", result);
+
+    ArrayList<Match> matches = matchMapper.selectAllByMatches();
+    model.addAttribute("matches", matches);
+
     return "janken.html";
 
   }
-
-
-
-
-
-
 
   /**
    * POSTを受け付ける場合は@PostMappingを利用する /sample25へのPOSTを受け付けて，FormParamで指定された変数(input
@@ -89,18 +92,9 @@ public class JankenController {
   public String jankenpost(@RequestParam String username, ModelMap model) {
 
     model.addAttribute("username", username);
+
     return "janken.html";
   }
-
-
-
-
-
-
-
-
-
-
 
   /**
    *
@@ -114,13 +108,10 @@ public class JankenController {
     this.entry.addUser(loginUser);
     model.addAttribute("entry", this.entry);
 
+    ArrayList<User> users = userMapper.selectAllByUsers();
+    model.addAttribute("users", users);
+
     return "janken.html";
   }
-
-
-
-
-
-
 
 }
